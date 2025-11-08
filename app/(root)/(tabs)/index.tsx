f90240e1f8504016dccbc9cc002d8a902464fe5d
mohsinabbas123 <abbasmohsin789@gmail.com>
2025-11-08 17:16:30 +0500

    Profile
    add work on profile implemnt profile text and notifcation icon and avatar and edit or upload button and user prfile name

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -6,12 +6,9 @@ import { Link } from "expo-router";
 export default function App() {
   return (
 
-   <View className="flex-1 items-center justify-center bg-pink-800">
-    <Text className="text-3xl text-yellow-300 font-rubik-bold my-10 ">welcome to real state</Text>
-     <Link className="text-white" href="/sign-in">sign In</Link>
-     <Link className="text-white" href="/explore">explore</Link>
-     <Link className="text-white" href="/profile">profile</Link>
-     <Link className="text-white" href="/properties/1">Property</Link>
+   <View className="flex-1 items-center justify-center bg-white">
+    <Text className="text-3xl text-black font-rubik-bold my-10 ">welcome to real state</Text>
+    
     </View>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import React from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const profile = () => {
+  const handleLogout = async () => {};
+
+
   return (
-    <View>
-      <Text>profile</Text>
+   <SafeAreaView className='h-full bg-white'>
+    <ScrollView
+    showsVerticalScrollIndicator={false}
+    contentContainerClassName='pb-32 px-7'
+    >
+      <View className='flex flex-row items-center justify-between mt-15'>
+        <Text className='text-xl font-rubik-bold'>Profile </Text>
+        <Image source={icons.bell} className='size-5'/>  {/* this size decrease the size bell*/}
+
+       
+      </View>
+
+
+
+      <View className='flex-row justify-center  flex mt-5'>
+
+    <View className='flex flex-col items-center relative mt-5'>
+      <Image source={images.avatar} className='size-44 relative rounded-full'/>
+      <TouchableOpacity className='absolute bottom-11 right-2 '>
+        <Image source={icons.edit} className='size-9'/>
+      </TouchableOpacity>
+      <Text className='text-2xl font-rubik-bold mt-2'>Mohsin | Abbas</Text>
+      
     </View>
+
+
+      </View>
+
+
+     
+ 
+    </ScrollView>
+   </SafeAreaView>
   )
 }
 
